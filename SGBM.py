98600d8e79a8143ceb985d50c3405c737b0d5b1f
Sam Nosenzo <samnosenzo@gmail.com>
2018-04-02 09:44:25 -0400

    changes to SGBM

--- SGBM.py
+++ SGBM.py
@@ -7,20 +7,17 @@ Credits:
 	https://docs.opencv.org/trunk/dd/d53/tutorial_py_depthmap.html
 	 
 """
+
+from stereovision import calibration
 import numpy as np
 import cv2
 import csv
 from matplotlib import pyplot as plt
 
-def calibration_station(img, mtx, dist):
-	h, w = img.shape[:2]
-	newcameramtx,roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-	dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
-	# mapx,mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
-	# dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
-	x, y, w, h = roi
-	det = dst[y:y+h, x:x+w]
-	return(dst)
+def calibration_station(imglrtup):
+	calib = calibration.StereoCalibration(input_folder='./calibration/calibfile/')
+	newframes = calib.rectify(imglrtup)
+	return newframes
 
 def plot_results(imgL, imgR, disparity):
 	plt.subplot(2,2,1)
@@ -36,9 +33,9 @@ def plot_results(imgL, imgR, disparity):
 
 def main():
 	# Load images
-	imgL = cv2.imread('img_2018-03-20_11_11_44__left.jpg',0)
-	imgR = cv2.imread('img_2018-03-20_11_11_44__right.jpg',0)
-
+	imgL = cv2.imread('Left1.jpg',0)
+	imgR = cv2.imread('Right1.jpg',0)
+	(imgL, imgR) = calibration_station((imgL, imgR))
 	# Apply calibration matricies
 	# dict_of_matricies = np.load('./calibration/calib_mats.npz')
 	# imgL = calibration_station(imgL, dict_of_matricies['intrinsic_mtx_l'], dict_of_matricies['dist_l'])
@@ -50,6 +47,18 @@ def main():
 	# 		writer.writerow([key, value])
 
 	# Get stereoSGBM Map
+
+    # block_matcher = StereoSGBM()
+    
+     
+    # camera_pair = CalibratedPair(None,
+    #                             StereoCalibration(input_folder='./calibration/calibfile/'),
+    #                             block_matcher)
+    # rectified_pair = camera_pair.calibration.rectify(image_pair)
+    # points = camera_pair.get_point_cloud(rectified_pair)
+    # points = points.filter_infinity()
+    # points.write_ply('outpc.ply')
+	
 	stereo = cv2.StereoSGBM_create(minDisparity=0,
 							  numDisparities=64, 
 							  blockSize=5,
