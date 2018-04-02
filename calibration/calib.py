df8b5a0f45c90da36b5aadc2df2c2f615a755dde
Sam Nosenzo <samnosenzo@gmail.com>
2018-04-02 09:39:54 -0400

    added calibration script

---
+++ calibration/calib.py
@@ -0,0 +1,33 @@
+from stereovision import calibration
+import numpy as np
+import cv2
+import glob2 as glob
+import argparse
+
+
+
+images_right = glob.glob('./right/*.jpg')
+print(images_right)
+images_left = glob.glob('./left/*.jpg')
+images_left.sort()
+images_right.sort()
+
+
+img_l = cv2.imread(images_left[0])
+img_h, img_w, img_chan = img_l.shape
+calib = calibration.StereoCalibrator(rows=6, columns=8, square_size=2.5, image_size=(img_w,img_h))
+
+for i, img in enumerate(images_left):
+	img_l = cv2.imread(images_left[i])
+	img_r = cv2.imread(images_right[i])
+
+	calib.add_corners((img_l, img_r))
+
+
+calibration = calib.calibrate_cameras()
+
+print("Error:\n")
+print(calib.check_calibration(calibration))
+
+calibration.export('./calibfile/');
+
