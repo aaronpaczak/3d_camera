adede5e189b219449974e9b4270eb86314512d73
Sam Nosenzo <samnosenzo@gmail.com>
2018-04-11 21:21:20 -0400

    changes to sgbm tester

--- sgbmTester.py
+++ sgbmTester.py
@@ -1,9 +1,10 @@
 from Recon3DUtil import *
-from cameraUtil import *
+# from cameraUtil import *
 
-io_cam_setup()
-captureStereo()
+# io_cam_setup()
+# captureStereo()
 images = getNewestImages()
 recImages = calibration_station(images)
 disp = depthMapSGBM(images)
+# plot_results(recImages[0], recImages[1], disp)
 save_results(disp)
